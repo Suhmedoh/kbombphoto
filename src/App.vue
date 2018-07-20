15a051e0bb73d5694f2679c5c86eb4e09f6f97a1
Josh Semedo <joshsemedo@gmail.com>
2018-07-20 12:59:18 -0400

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/steve.png">
+    <Gallery msg="The World Enders"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Gallery from './components/Gallery.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Gallery
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import url('https://fonts.googleapis.com/css?family=Fruktur|Merienda');
+    @font-face {
+    	font-family: 'Outrun future';
+    	src: url('~src/assets/fonts/Outrun-future.odt');
+    }
+    html {
+        background: #272727;
+    }
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #be7b7c;
+      background: #272727;
+      margin-top: 60px;
+    }
+    h1 {
+        font-family: 'Outrun future';
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VuePreview from 'vue-preview'
 
 Vue.config.productionTip = false
+Vue.use(VuePreview)
 
 new Vue({
   render: h => h(App)
